0312ae7890cdf2d052025f6af71188efced37cae
H-Yeji <yeji980603@naver.com>
2023-11-24 17:56:04 +0900

    회원 레퍼지토리 수정

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,15 +3,20 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository //컴포넌트 스캔에서 자동으로 스프링 빈으로 등록해줌
+@RequiredArgsConstructor
 public class MemberRepository {
 
+    private final EntityManager em;
+    /*
     @PersistenceContext //스프링이 엔티티 매니저를 만들어서 주입시켜줌 -> database와 연결
     private EntityManager em;
+    */
 
     public void save(Member member) { //저장
         em.persist(member);
