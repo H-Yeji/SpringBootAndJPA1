54dcd1dc5b08544958785d0d3e01a70d216a1f40
H-Yeji <yeji980603@naver.com>
2023-11-27 19:49:30 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -31,5 +31,63 @@ public class Order {
     private LocalDateTime orderDate; //*type Date->LocalDateTime
 
     @Enumerated(EnumType.STRING) //*
-    private OrderStatus orderStatus;
+    private OrderStatus status;
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER); //처음 상태를 order로 강제로 넣음
+        order.setOrderDate(LocalDateTime.now()); //주문 시간을 현재시간으로 세팅
+        return order;
+    }
+
+    //==비지니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) { //이미 배송 완료된 상품일 경우
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL); //위 예외를 통과하면 상태를 취소로 바꿔줌
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel(); //orderItem에도 cancel을 날려줘야 함
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum(); //orderItem에 만든 getTotalPrice 반환값을 더해줌
+
+        return totalPrice;
+    }
+
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -22,9 +22,29 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private int orderPrice;
+    private int orderPrice; //주문 가격
 
-    private int count;
+    private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //주문이 들어온만큼 재고를 차감함
+        return orderItem;
+    }
 
 
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); //주문 취소가 들어가면 주문 수량만큼 addStock해주어야 함
+    }
+
+    public int getTotalPrice() {
+
+        return getOrderPrice() * getCount(); //수량과 가격을 곱해서 총금액 반환
+    }
 }
