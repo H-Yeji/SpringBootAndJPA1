ccf56610f01870dacf0be745d8230dd6d015146a
H-Yeji <yeji980603@naver.com>
2023-11-27 18:15:16 +0900

    상품 엔티티 개발(비지니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,6 +27,25 @@ public abstract class Item { //*abstract 추가 - 추상 클래스
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+    /**
+     * stock 증가하는 로직
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /**
+     * stock 감소하는 로직
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) { //재고가 0보다 적을 시 예외 발생
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock; //통과하면 남은 수량으로 등록
+    }
+
 
 
 }
