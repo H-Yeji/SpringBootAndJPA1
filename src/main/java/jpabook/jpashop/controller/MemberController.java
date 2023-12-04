eef8de410d3fe8afc46f1969ce4856173705bca0
H-Yeji <yeji980603@naver.com>
2023-12-04 21:31:10 +0900

    회원 등록 컨트롤러 및 폼 개발

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원 등록 폼 열어봄
+     */
+    @GetMapping("/members/new") //이 주소로 get방식으로 들어오면 해당 메서드 실행
+    public String createForm(Model model) {
+        //controller -> view로 넘어갈 때 데이터를 데리고 넘어감
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     * 회원 등록을 진행
+     */
+    @PostMapping("members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/"; //리다이렉트로 첫번째 페이지로 돌아가도록 설정
+    }
+
+}
