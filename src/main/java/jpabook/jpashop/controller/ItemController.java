a073b9db772e0c7f321d941f0ffd3cb16acd44f1
H-Yeji <yeji980603@naver.com>
2023-12-05 21:04:31 +0900

    상품 수정 코드 개발

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.controller;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -18,6 +21,7 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
     /**
      * 아이템 등록 폼 열기
@@ -52,7 +56,7 @@ public class ItemController {
     }
 
     /**
-     * 상품 목록 조회;
+     * 상품 목록 조회
      */
     @GetMapping("/items")
     public String itemList(Model model) {
@@ -61,6 +65,45 @@ public class ItemController {
         model.addAttribute("items", items);
 
         return "items/itemList";
+    }
+
+    /**
+     * 상품 수정 폼
+     */
+    @GetMapping("/items/{itemId}/edit") // {}로 변경값 변수명 넣어 사용하는게 pathVariable
+    public String updateItemForm(@PathVariable Long itemId, Model model) {
+
+        Book item = (Book) itemService.findOne(itemId); //책만 사용한다는 가정
+
+        //기존에 저장된 데이터들 그대로 가지고 와서 폼에서 보여줌
+        BookForm form = new BookForm(); //수정을 위해 book 엔티티가 아니라 기존의 bookform 데이터를 보냄
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * 상품 수정 실행
+     */
+    @PostMapping("items/{itemId}/edit")
+    public String edit(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items"; //수정  완료 후 리다이렉트로 목록 띄워줌
     }
 
 }
